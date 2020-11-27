f4dcf9e6d172e002b968bba7c3044067749df909
fengxuezhi <fengxuezhi@outlook.com>
2020-11-27 20:42:18 +0800

    三数相加、四数相加归纳总结（双指针），多数之和

---
+++ LeetCode/18.四数之和.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode.cn id=18 lang=python3
+#
+# [18] 四数之和
+#
+# https://leetcode-cn.com/problems/4sum/description/
+#
+# algorithms
+# Medium (39.34%)
+# Likes:    679
+# Dislikes: 0
+# Total Accepted:    137.9K
+# Total Submissions: 350.3K
+# Testcase Example:  '[1,0,-1,0,-2,2]\n0'
+#
+# 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c
+# + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+# 
+# 注意：
+# 
+# 答案中不可以包含重复的四元组。
+# 
+# 示例：
+# 
+# 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+# 
+# 满足要求的四元组集合为：
+# [
+# ⁠ [-1,  0, 0, 1],
+# ⁠ [-2, -1, 1, 2],
+# ⁠ [-2,  0, 0, 2]
+# ]
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        """
+        4数之和、3数之和、N数之和统一解法：
+        先排序、再reduce到只剩2数之和、2数之和使用双指针来处理
+
+        【Tip】 排序+双指针
+
+        Time Complexity: O(n^3)
+        Space Complexity: O(n^3)
+        """
+        def findNSum(l: int, r: int, target: int, N: int, tmp_ans: List[int], ans: List[int]):
+            if r - l + 1 < N or N < 2 or target < nums[l] * N or target > nums[r] * N: # 早停 early termination
+                return
+            
+            if N == 2: # 双指针夹逼two pointers solve sorted 2-sum problem
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        ans.append(tmp_ans + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l - 1]: # 跳过连续相同值skip same value
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else: # 递归recursively reduce N
+                for i in range(l, r + 1):
+                    if i == l or (i > l and nums[i - 1] != nums[i]):
+                        findNSum(i + 1, r, target - nums[i], N - 1, tmp_ans + [nums[i]], ans)
+        
+        nums.sort()
+        ans = []
+        findNSum(0, len(nums) - 1, target, 4, [], ans)
+        return ans
+# @lc code=end
+
---
+++ LeetCode/454.四数相加-ii.py
@@ -0,0 +1,66 @@
+#
+# @lc app=leetcode.cn id=454 lang=python3
+#
+# [454] 四数相加 II
+#
+# https://leetcode-cn.com/problems/4sum-ii/description/
+#
+# algorithms
+# Medium (56.73%)
+# Likes:    286
+# Dislikes: 0
+# Total Accepted:    48K
+# Total Submissions: 83.5K
+# Testcase Example:  '[1,2]\n[-2,-1]\n[-1,2]\n[0,2]'
+#
+# 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] +
+# D[l] = 0。
+# 
+# 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -2^28 到 2^28 - 1
+# 之间，最终结果不会超过 2^31 - 1 。
+# 
+# 例如:
+# 
+# 
+# 输入:
+# A = [ 1, 2]
+# B = [-2,-1]
+# C = [-1, 2]
+# D = [ 0, 2]
+# 
+# 输出:
+# 2
+# 
+# 解释:
+# 两个元组如下:
+# 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+# 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        """
+        1. 一采用分为两组，HashMap 存一组，另一组和 HashMap 进行比对。
+        2. 这样的话情况就可以分为三种：
+            HashMap 存一个数组，如 A。然后计算三个数组之和，如 BCD。时间复杂度为：O(n)+O(n^3)，得到 O(n^3).
+            HashMap 存三个数组之和，如 ABC。然后计算一个数组，如 D。时间复杂度为：O(n^3)+O(n)，得到 O(n^3).
+            HashMap存两个数组之和，如AB。然后计算两个数组之和，如 CD。时间复杂度为：O(n^2)+O(n^2)，得到 O(n^2).
+        3. 根据第二点我们可以得出要存两个数组算两个数组。
+        4. 我们以存 AB 两数组之和为例。首先求出 A 和 B 任意两数之和 sumAB，以 sumAB 为 key，sumAB 出现的次数为 value，存入 hashmap 中。
+
+        【Tip】 Counter的应用
+
+        代码来自LeetCode海外版的光头哥！牛B!!
+
+        Time Complexity: O(n^2)
+        Space Complexity: O(n^2)
+        """
+        AB = collections.Counter(a + b for a in A for b in B)
+        return sum(AB[-c - d] for c in C for d in D)
+
+        
+# @lc code=end
+
